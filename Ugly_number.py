b274b11580b2d1cb61e0c6de0ba97484e88c3751
sarath805 <80090716+sarath805@users.noreply.github.com>
2022-06-13 21:57:52 +0530

    def is_ugly(N):     while N % 2 == 0:         N = N // 2     if N == 1:         return True     while N % 3 == 0:         N = N // 3     if N == 1:         return True     while N % 5 == 0:         N = N // 5     if N == 1:         return True     return False
    def yes(n):
        if is_ugly(n) == True:
            print("Ugly Number")
        else:
            print("Not Ugly Number")
            
    a = int(input())
    yes(a)

---
+++ Ugly_number.py
@@ -0,0 +1,23 @@
+def is_ugly(N):
+    while N % 2 == 0:
+        N = N // 2
+    if N == 1:
+        return True
+    while N % 3 == 0:
+        N = N // 3
+    if N == 1:
+        return True
+    while N % 5 == 0:
+        N = N // 5
+    if N == 1:
+        return True
+    return False
+    
+def yes(n):
+    if is_ugly(n) == True:
+        print("Ugly Number")
+    else:
+        print("Not Ugly Number")
+        
+a = int(input())
+yes(a)
