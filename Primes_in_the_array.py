0c0fee4dcf85c65db17be747522b8ad4d82b2530
sarath805 <80090716+sarath805@users.noreply.github.com>
2022-06-16 23:19:34 +0530

    def prime(n):     sum = 0     for i in range(1,n+1):         if n%i == 0:             sum = sum+1
        if sum == 2:
            return True
        else:
            return False
            
    a = int(input())
    x = list(map(int,input().split()))
    c = 0
    for i in x:
        if prime(i) == True :
            c = c+1
            
    print(c)

--- Primes_in_the_array.py
+++ Primes_in_the_array.py
@@ -1,19 +1,19 @@
-def prim(n):
-    s = 0
+def prime(n):
+    sum = 0
     for i in range(1,n+1):
-        if n%i ==0:
-            s = s+1
-    if s==2:
+        if n%i == 0:
+            sum = sum+1
+            
+    if sum == 2:
         return True
     else:
         return False
         
 a = int(input())
 x = list(map(int,input().split()))
-
-sum = 0
-
+c = 0
 for i in x:
-    if prim(i)==True :
-        sum = sum+1
-print(sum)
+    if prime(i) == True :
+        c = c+1
+        
+print(c)
