30546e8ce4fdd043b0a74802fbf0fb6d5e731dfb
sarath805 <80090716+sarath805@users.noreply.github.com>
2022-06-17 21:29:08 +0530

    a = int(input()) x = list(map(int,input().split())) z = [] for i in x:     if x.count(i) == i and i not in z:         z.append(i)
    if len(z)==0:
        print(-1)
    else:
        print(*z)

---
+++ Elements_same_as_count.py
@@ -0,0 +1,11 @@
+a = int(input())
+x = list(map(int,input().split()))
+z = []
+for i in x:
+    if x.count(i) == i and i not in z:
+        z.append(i)
+
+if len(z)==0:
+    print(-1)
+else:
+    print(*z)
