daa1770f632199c71c64c5e4d7976a3b4b0f63b9
sarath805 <80090716+sarath805@users.noreply.github.com>
2022-06-17 21:04:44 +0530

    a,b = map(int,input().split()) x = list(map(int,input().split())) y = list(map(int,input().split()))
    s1 = set(x)
    s2 = set(y)
    
    ass = s1.symmetric_difference(s2)
    print(len(ass))

--- Count_elements_II.py
+++ Count_elements_II.py
@@ -1,9 +1,9 @@
-m,n = map(int,input().split())
+a,b = map(int,input().split())
 x = list(map(int,input().split()))
 y = list(map(int,input().split()))
 
-n1 = set(x)
-n2 = set(y)
+s1 = set(x)
+s2 = set(y)
 
-a = n1.symmetric_difference(n2)
-print(len(a))
+ass = s1.symmetric_difference(s2)
+print(len(ass))
