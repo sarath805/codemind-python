103448f36f1ad09cbf6e76d72fa805d039d35894
sarath805 <80090716+sarath805@users.noreply.github.com>
2022-09-06 14:48:47 +0530

    def prime(n):     s = 0     for i in range(1,n+1):         if n%i == 0:             s =s+1     if s == 2:         return True     else:         return False def nxt(n):     while prime(n) == False:         n = n+1     return n def bef(n):     while prime(n) == False:         n = n-1     return n def near(n):     a = nxt(n)     b = bef(n)     if (a-n) >= (n-b):         print(b)     else:         print(a) for i in range(int(input())):     a = int(input())     near(a)

--- Nearest_Prime.py
+++ Nearest_Prime.py
@@ -1,32 +1,27 @@
-def prim(n):
+def prime(n):
     s = 0
     for i in range(1,n+1):
-        if n%i ==0:
-            s = s+1
+        if n%i == 0:
+            s =s+1
     if s == 2:
         return True
     else:
         return False
-        
 def nxt(n):
-    while prim(n) == False:
+    while prime(n) == False:
         n = n+1
     return n
-
-def beg(n):
-    while prim(n)==False:
+def bef(n):
+    while prime(n) == False:
         n = n-1
     return n
-    
 def near(n):
     a = nxt(n)
-    b = beg(n)
-    if (a-n)<(n-b):
-        print(a)
-    else:
+    b = bef(n)
+    if (a-n) >= (n-b):
         print(b)
-        
-c = int(input())
-for i in range(c):
-    x = int(input())
-    near(x)
+    else:
+        print(a)
+for i in range(int(input())):
+    a = int(input())
+    near(a)
