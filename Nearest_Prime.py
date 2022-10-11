bbf707b0c7bde63fb00ff798ca615fca0255057d
sarath805 <80090716+sarath805@users.noreply.github.com>
2022-10-11 14:36:31 +0530

    for i in range(int(input())):     a = int(input())     #before prime     k = a     p = 0     while k>0:         s = 0         for i in range(1,k+1):             if k%i == 0:                 s = s+1         if s == 2:             p = p+k             break         k = k-1     #after prime     q = 0     m = a     while k>0:         s = 0         for j in range(1,m+1):             if m%j == 0:                 s = s+1         if s == 2:             q = m             break         m = m+1     #near prime     if abs(a-p) <= abs(a-q):         print(p)     else:         print(q)

--- Nearest_Prime.py
+++ Nearest_Prime.py
@@ -1,27 +1,31 @@
-def prime(n):
-    s = 0
-    for i in range(1,n+1):
-        if n%i == 0:
-            s =s+1
-    if s == 2:
-        return True
-    else:
-        return False
-def nxt(n):
-    while prime(n) == False:
-        n = n+1
-    return n
-def bef(n):
-    while prime(n) == False:
-        n = n-1
-    return n
-def near(n):
-    a = nxt(n)
-    b = bef(n)
-    if (a-n) >= (n-b):
-        print(b)
-    else:
-        print(a)
 for i in range(int(input())):
     a = int(input())
-    near(a)
+    #before prime
+    k = a
+    p = 0
+    while k>0:
+        s = 0
+        for i in range(1,k+1):
+            if k%i == 0:
+                s = s+1
+        if s == 2:
+            p = p+k
+            break
+        k = k-1
+    #after prime
+    q = 0
+    m = a
+    while k>0:
+        s = 0
+        for j in range(1,m+1):
+            if m%j == 0:
+                s = s+1
+        if s == 2:
+            q = m
+            break
+        m = m+1
+    #near prime
+    if abs(a-p) <= abs(a-q):
+        print(p)
+    else:
+        print(q)
